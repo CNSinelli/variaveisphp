da60a5504557f1c54ca6fd9d2d289086201e4bee
Nicolas Sinelli <125403389+CNSinelli@users.noreply.github.com>
2024-03-09 15:59:02 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula</title>
+</head>
+<body>
+    <?php 
+	if(isset($_GET['operacao']) && isset($_GET['x']) && isset($_GET['y'])){
+
+		if($_GET['operacao'] == "adicao"){
+			echo $_GET['x'] + $_GET['y']
+		}
+		else if($_GET['operacao'] == "subtracao"){	
+			echo $_GET['x'] - $_GET['y']
+		}
+		else if($_GET['operacao'] == "multiplicacao"){
+			echo $_GET['x'] * $_GET['y']
+		}
+		else if($_GET['operacao'] == "divisao"){
+			echo $_GET['x'] / $_GET['y']
+		}
+		else{
+			echo "Nenhuma operação selecionada."
+		}
+	}
+    ?>
+</body>
+</html> 
